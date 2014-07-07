c92e91a4c074872b870ddb6e3b337322afa9c6ce
rtr14 <rtr14@naver.com>
2014-07-07 10:43:49 +0900

    start

---
+++ src/fsw_sensor_spi.c
@@ -0,0 +1,28 @@
+/** @file fsw_sensor_spi.c
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <dev/spi.h>
+#include <dev/ap7/cache.h>
+#include <dev/ap7/addrspace.h>
+#include <freertos/task.h>
+#include <freertos/semphr.h>
+#include <util/log.h>
+
+static inline void xmit_spi(BYTE dat) {
+	spi_write(sd_chip, dat);
+	spi_read(sd_chip);
+	printf("Write o: %x\r\n", dat);
+}
+
+static inline BYTE rcvr_spi(void) {
+	spi_write(sd_chip, 0xFF);
+	BYTE c = spi_read(sd_chip);
+	printf("Read %x\r\n", c);
+}
+
+
+
