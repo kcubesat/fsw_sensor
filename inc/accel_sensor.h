9f1e72f8b38351f301fd57003f729bad40842c28
rtr14 <rtr14@naver.com>
2014-07-08 14:17:40 +0900

    14/07/08

---
+++ inc/accel_sensor.h
@@ -0,0 +1,30 @@
+#ifndef ACCEL_SENSOR_H_
+#define ACCEL_SENSOR_H_
+
+#include <dev/spi.h>
+
+/**
+ * Helper function to setup a spi_chip_t.
+ * This will setup the chip select in the correct SPI mode and speed
+ * to communicate with the accel_sensor.
+ * @param spi_dev pointer to a spi device (must be initialised)
+ * @param spi_chip pointer to a spi chip where the settings are stored
+ * @param cs the chip select of the current accel_sensor
+ */
+void accel_sensor_spi_setup_cs(spi_dev_t * spi_dev, spi_chip_t * spi_chip, uint8_t cs);
+
+/**
+ * Get ACCEL_SENSOR temperature
+ * @param chip
+ * @return temperature in deg. C
+ */
+float accel_sensor_read_temp(spi_chip_t * chip);
+
+/**
+ * Get ACCEL_SENSOR temperature in RAW
+ * @param chip
+ * @return temperature in RAW
+ */
+int16_t accel_sensor_read_raw(spi_chip_t * chip);
+
+#endif /* ACCEL_SENSOR_H_ */
--- src/accel_sensor.c
+++ src/accel_sensor.c
@@ -10,7 +10,7 @@
 #include <freertos/queue.h>
 
 /* reference lm70 */
-void temp_sensor_spi_setup_cs(spi_dev_t * spi_dev, spi_chip_t * spi_chip, uint8_t cs) {
+void accel_sensor_spi_setup_cs(spi_dev_t * spi_dev, spi_chip_t * spi_chip, uint8_t cs) {
 	spi_chip->spi_dev = spi_dev;	//A pointer to the physical device SPI0
 	spi_chip->baudrate = 1000000;	//This is only the initial baud rate, it will be increased by the driver
 	spi_chip->spi_mode = 3;		// SPI mode
@@ -23,7 +23,7 @@ void temp_sensor_spi_setup_cs(spi_dev_t * spi_dev, spi_chip_t * spi_chip, uint8_
 	spi_setup_chip(spi_chip);
 }
 
-float temp_sensor_read_temp(spi_chip_t * chip) {
+float accel_sensor_read_temp(spi_chip_t * chip) {
 	if (spi_lock_dev(chip->spi_dev) < 0)
 		return 0;
 	spi_write(chip, 0x0000);
@@ -32,7 +32,7 @@ float temp_sensor_read_temp(spi_chip_t * chip) {
 	return (((double)(temp >>5)) / 4.0);
 }
 
-int16_t temp_sensor_read_raw(spi_chip_t * chip) {
+int16_t accel_sensor_read_raw(spi_chip_t * chip) {
 	if (spi_lock_dev(chip->spi_dev) < 0)
 		return 0;
 	spi_write(chip, 0x0000);
