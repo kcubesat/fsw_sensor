29940aaf7f76f1808d29bab47ac97b330f6e9e01
rtr14 <rtr14@naver.com>
2014-07-10 11:39:39 +0900

    14/07/10

--- src/accel_sensor.c
+++ src/accel_sensor.c
@@ -29,7 +29,7 @@
 
 
 /* reference gyro */
-/*
+
 static void accel_sensor_write_reg(spi_chip_t * chip, uint8_t reg_base, uint16_t value) {
 	if (spi_lock_dev(chip->spi_dev) < 0)
 		return;
@@ -42,7 +42,7 @@ static void accel_sensor_write_reg(spi_chip_t * chip, uint8_t reg_base, uint16_t
 	spi_unlock_dev(chip->spi_dev);
 }
 
-static uint16_t accel_read_reg(spi_chip_t * chip, uint8_t reg_base) {
+static uint16_t accel_sensor_read_reg(spi_chip_t * chip, uint8_t reg_base) {
 	uint16_t val;
 	if (spi_lock_dev(chip->spi_dev) < 0)
 		return 0;
@@ -55,8 +55,8 @@ static uint16_t accel_read_reg(spi_chip_t * chip, uint8_t reg_base) {
 	spi_unlock_dev(chip->spi_dev);
 	return val;
 }
-*/
 
+/*
 static void accel_sensor_write_reg(spi_chip_t * chip, uint8_t reg_base) {
 	if (spi_lock_dev(chip->spi_dev) <0)
 		return;
@@ -79,12 +79,12 @@ static uint16_t accel_sensor_read_reg(spi_chip_t * chip, uint8_t reg_base) {
 	spi_unlock_dev(chip->spi_dev);
 	return val;
 }
-
+*/
 
 /* reference lm70 */
 void accel_sensor_spi_setup_cs(spi_dev_t * spi_dev, spi_chip_t * spi_chip, uint8_t cs) {
 	spi_chip->spi_dev = spi_dev; 	// A pointer to the physical device SPI0
-	spi_chip->baudrate = 1000000; 	// This is only initial baud rate, it will be increased by the driver
+	spi_chip->baudrate = 100000; 	// This is only initial baud rate, it will be increased by the driver
 	spi_chip->spi_mode = 3;		// SPI mode
 	spi_chip->bits = 16;		// Default value for transferring bytes
 	spi_chip->cs = cs; 		// The chip select number
@@ -96,21 +96,19 @@ void accel_sensor_spi_setup_cs(spi_dev_t * spi_dev, spi_chip_t * spi_chip, uint8
 }
 
 void accel_sensor_spi_setup(spi_chip_t * chip) {
-	int8_t in_byte = accel_sensor_read_reg(chip, ACCEL_SENSOR_CTRL_WHO_AM_I) & 0xFFFF;  //Read register WHO_AM_I
-	printf("WHO_AM_I : %f\r\n", in_byte);
-	//int8_t in_byte = accel_sensor_read_reg(chip, ACCEL_SENSOR_CTRL_WHO_AM_I);  //Read register WHO_AM_I
-	//printf("WHO_AM_I : %h", in_byte);
-
-	accel_sensor_write_reg(chip, ACCEL_SENSOR_CTRL_REG1);
+//	accel_sensor_write_reg(chip, ACCEL_SENSOR_CTRL_?????, ??? );
+	printf("WHO_AM_I : %d\r\n", accel_sensor_read_reg(chip, ACCEL_SENSOR_CTRL_WHO_AM_I));
 }
 
 float accel_sensor_read_accel(spi_chip_t * chip) {
 //	if (spi_lock_dev(chip->spi_dev) < 0)
 //		return 0;
 	x_val_t * x_val; //, y_val, z_val;
+//	int8_t val[1] = {accel_sensor_read_reg(chip, ACCEL_SENSOR_DATA_ACCEL_X_OUT_LOW), accel_sensor_read_reg(chip, ACCEL_SENSOR_DATA_ACCEL_X_OUT_HIGH)};
+
 	x_val->low = accel_sensor_read_reg(chip, ACCEL_SENSOR_DATA_ACCEL_X_OUT_LOW); // low out x 
 	x_val->high = accel_sensor_read_reg(chip, ACCEL_SENSOR_DATA_ACCEL_X_OUT_HIGH); // low out x 
-	printf("x=%d", x_val);	
+	printf("x=%d\r\n",x_val);	
 //	int16_t y_val = accel_sensor_read_reg(chip, ACCEL_SENSOR_DATA_ACCEL_Y_OUT_LOW); // low out y
 //	int16_t z_val = accel_sensor_read_reg(chip, ACCEL_SENSOR_DATA_ACCEL_Z_OUT_LOW); // low out z
 	return 0; //, y_val, z_val;
